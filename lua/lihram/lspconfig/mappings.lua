8271b39c07e6e5cc146063ae1c391e7185a64266
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2022-01-10 13:57:16 +0100

    feat: correctly impl mappings

--- lua/lihram/lspconfig/mappings.lua
+++ lua/lihram/lspconfig/mappings.lua
@@ -7,22 +7,27 @@ local on_attach = function(client, bufnr)
 	
 	local opts = { noremap=true, silent=true }
 
-	buf_set_keymap("n", "<leader>gg", buf.definition, opts)
-	buf_set_keymap("n", "<leader>gi", buf.definition, opts)
-	buf_set_keymap("n", "<leader>gD", buf.declaration, opts)
+	buf_set_keymap("n", "<leader>gg", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "<leader>gi", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 
-	buf_set_keymap("n", "<leader>gsh", require("lspsaga.signaturehelp").signature_help, opts)
-	buf_set_keymap("n", "<c-\\>", require("lspsaga.signaturehelp").signature_help, opts)
+	buf_set_keymap("n", "<leader>gsh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<c-\\>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
-	buf_set_keymap("n", "<leader>grr", buf.references, opts)
-	buf_set_keymap("n", "<leader>grn", buf.rename, opts)
-	buf_set_keymap("n", "<leader>gh", buf.hover, opts)
-	buf_set_keymap("n", "<leader>gca", buf.code_action, opts)
-	buf_set_keymap("n", "<leader>gdn", diag.goto_next, opts)
-	buf_set_keymap("n", "<leader>gdp", diag.goto_prev, opts)
+	buf_set_keymap("n", "<leader>grr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "<leader>grn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "<leader>gca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "<leader>gdn", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<leader>gdp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local servers = { "rust_analyzer" }
+local servers =
+	{ "rust_analyzer"
+	, "omnisharp"
+	, "yamlls"
+	}
 
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup {
@@ -32,4 +37,3 @@ for _, lsp in ipairs(servers) do
 		}
 	}
 end
-
