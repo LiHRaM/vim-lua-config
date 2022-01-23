a4e10d90ad50c9c7d54afe7560aee037395e7585
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2022-01-23 18:34:23 +0100

    feat: add nix flake

--- lua/lihram/init.lua
+++ lua/lihram/init.lua
@@ -1,6 +1,6 @@
+require("lihram.plugins")
 require("lihram.treesitter")
 require("lihram.lspconfig")
-require("lihram.plugins")
 require("lihram.keymappings")
 require("lihram.options")
 require("lihram.nvim-cmp")
