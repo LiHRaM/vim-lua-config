6f3a3a4ca73a47c7f1009872903b3145d3cf38ad
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2022-01-28 15:00:27 +0100

    feat: add Telescope keybindings to plugin settings

--- lua/lihram/plugins/telescope.lua
+++ lua/lihram/plugins/telescope.lua
@@ -11,3 +11,10 @@ require("telescope").setup {
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require("telescope").load_extension("ui-select")
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<Leader>ff", ":Telescope find_files<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>fb", ":Telescope buffers<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>fgb", ":Telescope git_branches<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>fgc", ":Telescope git_commits<CR>", opts)
+
