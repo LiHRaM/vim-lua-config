a4f6a766f1abfdfb8142aaa78fc316f9ef7c0ab2
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2022-01-28 12:53:09 +0100

    fix: remove references to lspsaga in lspconfig

--- lua/lihram/plugins/lspconfig/config.lua
+++ lua/lihram/plugins/lspconfig/config.lua
@@ -1,10 +1,5 @@
 local lsp = vim.lsp
 
-lsp.handlers["textDocument/hover"] = lsp.with(
-  require("lspsaga.hover").handler,
-  { border = "single" }
-)
-
 lsp.handlers["textDocument/signatureHelp"] = lsp.with(
   lsp.handlers.signature_help,
   { border = "single" }
